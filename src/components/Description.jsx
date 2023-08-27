5b8a93c9987b6c3f4e488c25c9b375aa1565597a
Zaid Ahmad <zaidd250@gmail.com>
2023-08-27 12:32:30 +0530

    fix product detail page for mobile devices

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -2,11 +2,11 @@
 function Description({ product }) {
   return (
     <>
-      <div className='container'>
+      <div className='container mt-5 md:mt-0'>
         <h3 className=' font-roboto text-gray-800 pb-3 font-medium'>
           Product details
         </h3>
-        <div className='w-3/5'>
+        <div className='w-full md:w-3/5'>
           <div className='text-gray-600'>{product.description}</div>
 
           <table className='table-auto border-collapse w-full text-left text-gray-600 text-sm mt-6'>
